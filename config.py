9cfcbb423f7e888f3e2789c39475758c71d4e19c
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-26 04:19:52 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,69 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_id = int(os.getenv("API_ID", "29486311"))
+api_hash = os.getenv("API_HASH", "ffdc688dc4eee8d2585cb24155188432")
+bot_token = os.getenv("BOT_TOKEN", "")
+# =========================================================== #
+
+db_url = os.getenv("DB_URL", "mongodb+srv://ucik:ucik@cluster0.0l3r8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db_name = os.getenv("DB_NAME", "tt9") #bisa diganti sesuai kebutuhan
+# =========================================================== #
+
+channel_1 = int(os.getenv("CHANNEL_1", "-1001991260453"))
+channel_2 = int(os.getenv("CHANNEL_2", "-1001841428029")) #untuk group comentar user
+channel_log = int(os.getenv("CHANNEL_LOG", "-1002068950974"))
+# =========================================================== #
+
+id_admin = int(os.getenv("ID_ADMIN", "1613540894"))
+# =========================================================== #
+
+batas_kirim = int(os.getenv("BATAS_KIRIM", "2"))
+# =========================================================== #
+
+biaya_kirim = int(os.getenv("BIAYA_KIRIM", "50"))
+# =========================================================== #
+
+hastag = os.getenv("HASTAG", "#111 #112 #fwbboy #fwbgirl #fwbspill #fwbask #fwbstory").replace(" ", "|").lower()
+# =========================================================== #
+
+pic_boy = os.getenv("PIC_BOY", "https://telegra.ph/file/c67bd36023648dc777bd9.jpg")
+pic_girl = os.getenv("PIC_GIRL", "https://telegra.ph/file/cb885bcbf5081dbd45f27.jpg")
+# =========================================================== #
+
+pesan_join = os.getenv("PESAN_JOIN", """
+kamu tidak dapat menggunakan bot 🙅, untuk mengirim pesan ke @fwb18fess harap join terlebih dahulu ke channel dan grup yang ada dibawah, jika sudah tekan coba lagi atau /help.
+
+Seputar informasi & pertanyaan hubungi @leoservicebot
+🚫 NSFW = BAN PERMANENT.
+""")
+start_msg = os.getenv("START_MSG", """
+halo {mention}
+
+💬 Sender 18+ Fwb adalah bot promote yang dapat digunakan untuk mencari teman, pacar, fwb dll serta dapat digunakan untuk mengirim pap/video randommu, gunakan hastag dibawah untuk mengirim pesan:
+
+#fwbboy : untuk mencari teman jika kamu seorang cowo.
+#fwbgirl : untuk mencari teman jika kamu seorang cewe.
+#fwbspill : untuk spill sesuatu.
+#fwbstory : untuk berbagi cerita/pengalaman.
+#fwbask : untuk bertanya ke grup.
+
+🚫 NSFW = BAN PERMANENT.
+""")
+
+gagalkirim_msg = os.getenv("GAGAL_KIRIM", """
+{mention} pesanmu gagal terkirim 🙅, harap gunakan hastag : 
+
+#fwbboy : untuk mencari teman jika kamu seorang cowo.
+#fwbgirl : untuk mencari teman jika kamu seorang cewe.
+#fwbspill : untuk spill sesuatu.
+#fwbstory : untuk berbagi cerita/pengalaman.
+#fwbask : untuk bertanya ke grup.
+
+powered by @topbasetelegram
+🚫 NSFW = BAN PERMANENT.
+""")
+
+NON_MEMBER_PHOTO = os.getenv("NON_MEMBER_PHOTO", "https://i.imgur.com/ZQZSWrt.jpg")
